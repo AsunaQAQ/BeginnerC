964384211e0baabee78606b06d2459cb23aad297
AsunaQAQ <w1906435164@163.com>
2022-05-09 22:08:02 +0800

    day1

---
+++ Day1/Demo1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+	//printf("Hello Word!");
+
+	
+
+	int num = 1;
+	printf("Êä³ö%d\n", num);
+	printf("1:%zd", sizeof(int));
+	//printf("2:%d", sizeof(num));
+	//printf("3:%d", sizeof num);
+
+
+	//return 0;
+}
+
