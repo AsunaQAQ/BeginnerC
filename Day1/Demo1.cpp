acbdf0d87be3120ed9b51c9f77f04623e850f8ce
AsunaQAQ <w1906435164@163.com>
2022-05-11 17:38:09 +0800

    第二天

---
+++ Day1/Demo1.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include <cfloat>
+#define num
+
+//int funcation() {
+//
+//	int a = 10;
+//	int b = 20;
+//
+//	return a + b;
+//}
+
+int main() {
+
+	//printf("Hello Word!");
+	/*
+	printf("输出:%d\n", num);
+	printf("int:%zd\n",sizeof(int));
+	printf("float:%zd\n", sizeof(float));
+	printf("Float:%E\n", FLT_MIN);*/
+
+	//int totalvalue;
+	//num = 10;
+	//totalvalue = num * 10;
+	//printf("tatalvalue = %d\n",totalvalue);
+
+	/*int result;
+	result = funcation();
+	printf("输入function:%d", result);*/
+
+	/*const int wight = 100;
+	const int hight = 100;
+	const double pi = 3.1415926;
+
+	int arae;
+	arae = wight * hight;
+	printf("面积：%d",arae);*/
+
+	/*float a;
+	float b;
+	float c;
+	scanf_s("%f,%f,%f",&a,&b,&c);
+
+	printf("a:%f  b:%f  c:%f", a, b, c);*/
+
+	/*char a, b, c;
+	scanf_s("%c %c %c", &a, 1, &b, 1, &c, 1);
+	printf("%c %c %c",a, b, c);*/
+
+	//int s;
+	//scanf_s("%d",&s); //s = 23
+	//printf("%d\n",s); //输出的是23  是值
+	//printf("%d", &s); //输出的是地址 1056842054836
+
+
+	return 0;
+}
+
+
+
